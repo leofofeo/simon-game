b558417da201ccce4b91c3591c60636f60584a56
Leo Rubiano <leo.a.rubiano@gmail.com>
2017-06-06 17:43:38 -0400

    started adding Javascript functionality, wrapped up most styling

--- js/script.js
+++ js/script.js
@@ -3,3 +3,28 @@ $('document').ready(function(){
 
 
 });
+
+var StrictMode = false;
+var Start = false;
+var SequenceCount = 00;
+var ActiveGame = false;
+
+
+$('#on-off-btn').on('click', function(){
+	if($(this).hasClass('btn-off')){
+		$(this).removeClass('btn-off');
+		$(this).text('ON');
+		ActivateGame();
+	} else {
+		$(this).addClass('btn-off');
+		$(this).text('OFF');
+		DisactivateGame();
+	}
+});
+
+
+
+
+var ActivateGame = function(){}
+
+var DisactivateGame = function(){}
